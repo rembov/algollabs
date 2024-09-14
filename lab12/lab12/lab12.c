61320b11a3b2b2a5082867143c5ffe193e4b6674
rembov <i7888@mail.ru>
2024-09-14 22:40:34 +0300

    5 переделаноэ

--- lab12/lab12/lab12.c
+++ lab12/lab12/lab12.c
@@ -1,4 +1,5 @@
-﻿#include <stdlib.h>
+﻿#define _CRT_SECURE_NO_WARNINGS 
+#include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 #include <locale.h>
@@ -17,61 +18,130 @@ void vivod(struct student vvv) {
         printf("Факультет %s \n", stud[i].facult);
         printf("Номер зачетной книжки %d \n", stud[i].nomzach);
         printf("Средний балл %d \n", stud[i].sredbal);
+        printf("--------------------------------------------------------------------------------------------\n");
     }
 
 }
+#pragma warning(disable: 4996)
 struct student* sozdanie() {
     struct student* bakalavri = (struct student*)malloc(3 * sizeof(struct student));
-    for (int i = 0; i < 3; i++) {
-        printf("Введите фамилию : ");
-        scanf_s("%20s", stud[i].famil);
-        printf("Введите имя : ");
-        scanf_s("%20s", stud[i].name);
-        printf("Введите факультет : ");
-        scanf_s("%20s", stud[i].facult);
-        printf("Введите номер зачётки : ");
-        scanf_s("%u", &stud[i].nomzach);
-        printf("Введите средний балл : ");
-        scanf_s("%f", &stud[i].sredbal);
-    }
+   
+        for (int i = 0; i < 3; i++) {
+            printf("Введите фамилию студента %d\n", i + 1);
+            scanf("%s", stud[i].famil);
+            rewind(stdin);
+            printf("Введите имя студента %d\n", i + 1);
+            scanf("%s", stud[i].name);
+            rewind(stdin);
+            printf("Введите факультет студента %d\n", i + 1);
+            scanf("%s", stud[i].facult);
+            rewind(stdin);
+            printf("Введите номер зачётки студента %d\n", i + 1);
+            scanf("%d", &stud[i].nomzach);
+            rewind(stdin);
+            printf("Введите средний балл зачётки студента %d\n", i + 1);
+            scanf("%d", &stud[i].sredbal);
+            rewind(stdin);
+        }
     return bakalavri;
 };
 
-int poisk(struct student* src, int stud_num) {
+int poisk(struct student* src) {
     char name[20], famil[20], facult[20];
     unsigned int nomzach;
     float sredbal;
-    printf("Введите фамилию : ");
-    scanf_s("%s", famil);
-    printf("Введите имя : ");
-    scanf_s("%s", name);
-    printf("Введите факультет : ");
-    scanf_s("%s", facult);
-    printf("Введите номер зачётки : ");
-    scanf_s("%u", &nomzach);
-    printf("Введите средний балл : ");
-    scanf_s("%u", &sredbal);
-
-    for (int i = 0; i < 3; i++) {
-        if (strcmp(famil, stud[i].famil) == 0 && strcmp(name, stud[i].name) == 0 &&
-            strcmp(facult, stud[i].facult) == 0 && nomzach == stud[i].nomzach
-            ) {
-            printf("Студент найден:");
-            vivod(stud[i]);
-            return 0;
+    int n,k=0;
+    printf("Выберите критерий поиска:\n 1: Фамилия\n 2: Имя\n 3: Факультет\n 4: Номер зачетной книжки\n 5: средний балл\n");
+    scanf("%d", &n);
+    switch (n) {
+    case 1:
+        printf("Введите фамилию студента\n");
+        scanf("%20s", famil);
+        printf("Студенты, подходящте под заданные данные:\n");
+        for (int i = 0; i < 3; i++) {
+            if (strcmp(stud[i].famil,famil) == 0) {
+                printf("%s %s %s %d %d\n", stud[i].famil, stud[i].name, stud[i].facult, stud[i].nomzach, stud[i].sredbal);
+                k++;
+            }
         }
+        if (k == 0) {
+            printf("Ни одного студента не найдено\n");
+        }
+        break;
+    case 2:
+        printf("Введите имя студента\n");
+        scanf("%20s", name);
+        printf("Студенты, подходящте под заданные данные:\n");
+        for (int i = 0; i < 3; i++) {
+            if (strcmp(stud[i].name, name) == 0) {
+                printf("%s %s %s %d %d\n", stud[i].famil, stud[i].name, stud[i].facult, stud[i].nomzach, stud[i].sredbal);
+                k++;
+            }
+        }
+        if (k == 0) {
+            ("Ни одного студента не найдено\n");
+        }
+
+        break;
+    case 3:
+        printf("Введите факультет студента\n");
+        scanf("%20s", facult);
+        printf("Студенты, подходящте под заданные данные:\n");
+        for (int i = 0; i < 3; i++) {
+            if (strcmp(stud[i].facult,facult) == 0) {
+                printf("%s %s %s %d %d\n", stud[i].famil, stud[i].name, stud[i].facult, stud[i].nomzach, stud[i].sredbal);
+                k++;
+            }
+        }
+        if (k == 0) {
+            printf("Ни одного студента не найдено\n");
+        }
+        break;
+    case 4:
+        printf("Введите номер зачетки студента\n");
+        scanf("%d", &nomzach);
+        printf("Студенты, подходящте под заданные данные:\n");
+        for (int i = 0; i < 3; i++) {
+            if (stud[i].nomzach == nomzach) {
+                printf("%s %s %s %d %d\n", stud[i].famil, stud[i].name, stud[i].facult, stud[i].nomzach, stud[i].sredbal);
+                k++;
+            }
+        }
+        if (k == 0) {
+            printf("Ни одного студента не найдено\n");
+        }
+        break;
+    case 5:
+        printf("Введите номер зачетки студента\n");
+        scanf("%d", &sredbal);
+        printf("Студенты, подходящте под заданные данные:\n");
+        for (int i = 0; i < 3; i++) {
+            if (stud[i].sredbal == sredbal) {
+                printf("%s %s %s %d %d\n", stud[i].famil, stud[i].name, stud[i].facult, stud[i].nomzach, stud[i].sredbal);
+                k++;
+            }
+        }
+        if (k == 0) {
+            printf("Ни одного студента не найдено\n");
+        }
+        break;
+    default:
+        printf("Ошибка, выберите один из представленых пунктов");
+        break;
     }
-    printf("Такого студента не существует");
-    return 0;
 }
 int main()
 {
     setlocale(LC_ALL, "Rus");
     struct student* studd = sozdanie();
-    for (int i = 0; i < 3; i++) {
-        vivod(studd[i]);
-    }
-    poisk(studd, 3);
+    printf("-----------------------\n");
+    printf("--------------------------------\n");
+    printf("------------------------------------------\n");
+    printf("--------------------------------------------------\n");
+  
+        vivod(studd[3]);
+    
+    poisk(studd);
     free(studd);
     return 0;
 
