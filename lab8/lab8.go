132893f0fb47f73f0d0692bfcfa5c6b536fe3de5
rembov <i7888@mail.ru>
2024-11-12 14:37:09 +0300

    8

--- lab8/lab8.go
+++ lab8/lab8.go
@@ -65,55 +65,6 @@ func bfs(start int, matrix [][]int, visited []bool) {
 	}
 }
 
-func generateList(matrix [][]int) map[int][]int {
-	adjList := make(map[int][]int)
-	for i := 0; i < len(matrix); i++ {
-		for j := 0; j < len(matrix[i]); j++ {
-			if matrix[i][j] == 1 {
-				adjList[i] = append(adjList[i], j)
-			}
-		}
-	}
-	return adjList
-}
-
-func printList(adjList map[int][]int) {
-	for node, neighbors := range adjList {
-		fmt.Printf("%d: %v\n", node, neighbors)
-	}
-}
-
-func bfsList(adjList map[int][]int) {
-	visited := make(map[int]bool)
-
-	for v := range adjList {
-		if !visited[v] {
-			bfs1(v, adjList, visited)
-		}
-	}
-}
-
-func bfs1(start int, adjList map[int][]int, visited map[int]bool) {
-	queue := list.New()
-	queue.PushBack(start)
-	visited[start] = true
-
-	for queue.Len() > 0 {
-		element := queue.Front()
-		v := element.Value.(int)
-		queue.Remove(element)
-
-		fmt.Printf("%d ", v)
-
-		for _, neighbor := range adjList[v] {
-			if !visited[neighbor] {
-				queue.PushBack(neighbor)
-				visited[neighbor] = true
-			}
-		}
-	}
-}
-
 func main() {
 	rand.Seed(time.Now().UnixNano())
 
@@ -125,10 +76,4 @@ func main() {
 	fmt.Println("\nОбход в ширину (матрица смежности):")
 	bfsMatrix(matrix)
 
-	adjList := generateList(matrix)
-	fmt.Println("\n\nСписок смежности неориентированного графа:")
-	printList(adjList)
-
-	fmt.Println("\nОбход в ширину (список смежности):")
-	bfsList(adjList)
 }
